edeba9f95af2660a1ae46dd7f71614b6c6029f6f
parampreet bedi <parampreet.singh@smartdatainc.net>
2018-04-04 18:54:45 +0530

    eod4thapr

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -1,16 +1,19 @@
 var Attendance = require('../models/attendance');
 
 exports.save = function(req, res){
-    if(req.body.plan && req.body.date && req.body.classConducted && req.body.traineesPresent){
+    if(req.body.plan && req.body.classDate && (req.body.classConducted==0 || req.body.classConducted==1) && req.body.traineesPresent){
 		var atnd = {
 			plan:req.body.plan,
-			date:req.body.date,
+			date:req.body.classDate,
 			classConducted:req.body.classConducted,
 			traineesPresent:req.body.traineesPresent
 		}
 		Attendance(atnd).save(function(err){
 			if(!err){
 				res.status(200).jsonp({"msg":"Records saved successfully"});
+			}
+			else{
+				res.status(404).jsonp(err);
 			}
 		})
 	}else{
@@ -20,8 +23,7 @@ exports.save = function(req, res){
 
 exports.fetch = function(req, res){
     if(req.params.all=='all'){
-        Attendance.find().exec(function(err, atnd){
-            //console.log(atnd);
+        Attendance.find().populate('','').populate('','').exec(function(err, atnd){
             if(err){
                 res.status(404).jsonp(err)
             }else{
@@ -30,7 +32,6 @@ exports.fetch = function(req, res){
         }) 
     }else if(req.params.all){
         Attendance.findOne({_id:req.params.all}).exec(function(err, atnd){
-            //console.log(atnd);
             if(err){
                 res.status(404).jsonp(err)
             }else{
@@ -42,4 +43,3 @@ exports.fetch = function(req, res){
     }
 
 }
-
